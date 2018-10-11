345b58784f7a24760db17c636d7b2d6f644b5e24
JonathanLebourg <jonjonlebourg@gmail.com>
2018-10-11 10:16:59 +0200

    first commit

---
+++ exercicepartie3-3.js
@@ -0,0 +1,12 @@
+function green(){ //affiche le text en vert
+  document.getElementById("text").style.color = "green";
+  document.getElementById("exercice").style.backgroundColor = "green";
+}
+function red(){ //affiche le text en rouge
+  document.getElementById("text").style.color = "red";
+  document.getElementById("exercice").style.backgroundColor = "red";
+}
+function blue(){ //affiche le text en bleu
+  document.getElementById("text").style.color = "blue";
+  document.getElementById("exercice").style.backgroundColor = "blue";
+}
